2036dd338bc209279ce6df97be808d07b1b67b84
NguyenLeHuu <huunlse150800@fpt.edu.vn>
2023-02-19 20:13:01 +0700

    qua met

--- src/route/AgencyRoute.js
+++ src/route/AgencyRoute.js
@@ -1,9 +1,14 @@
-const express = require('express');
-const AgencyController = require('../controllers/AgencyController')
-const authenToken = require('../services/AuthenToken')
+const express = require("express");
+const AgencyController = require("../controllers/AgencyController");
+const AuthMiddleware = require("../middleware/AuthMiddleware");
 
-let route =  express.Router();
+let route = express.Router();
 
-route.get("/test",authenToken,AgencyController.index);
+route.get(
+  "/test",
+  AuthMiddleware.isAuthenticated,
+  AuthMiddleware.isAdmin,
+  AgencyController.index
+);
 
 module.exports = route;
