c0f2dade32dbfcd9e7a2f8e6d02edb16d3c4ff29
hoghanh <hanhchse150754@fpt.edu.vn>
2023-03-24 09:20:03 +0700

    update mobile

--- src/services/Firebase.js
+++ src/services/Firebase.js
@@ -84,7 +84,7 @@ const uicha_topic = "UICHA_TOPIC";
 //dang ki zo topic
 const registerCustomerTopic = function () {
   const registrationTokens = [
-    "eowymDzjzrDw-YfpzdJgJs:APA91bFpIgAosp6Z3SgHZr59sr2Juo9uJanEE7WtSw7Chp-bgNFcGyefVETfYJ1fuKhTDcmDmdtYd8TuyeALS_MkfIIyC-FU4q5gRfZCF6YHWkdfASqiE1Oul1NsS5GSFh8-hu0n545G",//huu
+    "eowymDzjzrDw-YfpzdJgJs:APA91bFpIgAosp6Z3SgHZr59sr2Juo9uJanEE7WtSw7Chp-bgNFcGyefVETfYJ1fuKhTDcmDmdtYd8TuyeALS_MkfIIyC-FU4q5gRfZCF6YHWkdfASqiE1Oul1NsS5GSFh8-hu0n545G", //huu
   ];
 
   admin
@@ -100,9 +100,9 @@ const registerCustomerTopic = function () {
 
 const registerCreatorTopic = function () {
   const registrationTokens = [
-    "cg9vLKO8RFaMBsW9LxELkB:APA91bFF0PM8MZOFQ_MQ4tcrEC7c6pNIRQ9s4JM7O388T75wfa5gEHDX8Il9MV7Bw79qh_MrcIJiWZ_grOcXwZ2O14SfRQMyCPDyNllJ_bNOevcig1hdh4ZvgcTBWEIIiaMNX0CQmLV0",//bảo mo
-    "fSvUQ007QASluFETXUCIdx:APA91bF_1MBgHxqFV5bf51pTXvCCS4eO3FRCIUZt-DQS6cNA0lDphCd7D6TI7Kqfzzj8DBJxg0IPqueLh8I8arDOVlPni0rXMbAW0OBbxGlZTYZI4RjlLM3VWZprhJmfVuV1FaWqtbQv",//
-    "eRVsNwobSVSttr8W3LT_CZ:APA91bEvQCsQhK4JM6yX5iZt3uahN-fin6y00D5j1_zbQPQ1BHVBpbcMcpnjhKZk0cFP8-BjQ2Vss1Fp6hdiAd4m496PUsP5gr1rz-L8pQi7UgWhkYpvNmjaFwU1PSr6kZf1HEVznx9e",//q.a
+    "cg9vLKO8RFaMBsW9LxELkB:APA91bFF0PM8MZOFQ_MQ4tcrEC7c6pNIRQ9s4JM7O388T75wfa5gEHDX8Il9MV7Bw79qh_MrcIJiWZ_grOcXwZ2O14SfRQMyCPDyNllJ_bNOevcig1hdh4ZvgcTBWEIIiaMNX0CQmLV0", //bảo mo
+    "fSvUQ007QASluFETXUCIdx:APA91bF_1MBgHxqFV5bf51pTXvCCS4eO3FRCIUZt-DQS6cNA0lDphCd7D6TI7Kqfzzj8DBJxg0IPqueLh8I8arDOVlPni0rXMbAW0OBbxGlZTYZI4RjlLM3VWZprhJmfVuV1FaWqtbQv", //
+    "eRVsNwobSVSttr8W3LT_CZ:APA91bEvQCsQhK4JM6yX5iZt3uahN-fin6y00D5j1_zbQPQ1BHVBpbcMcpnjhKZk0cFP8-BjQ2Vss1Fp6hdiAd4m496PUsP5gr1rz-L8pQi7UgWhkYpvNmjaFwU1PSr6kZf1HEVznx9e", //q.a
   ];
   admin
     .messaging()
@@ -118,8 +118,9 @@ const registerCreatorTopic = function () {
 const registerUichaTopic = function () {
   const registrationTokens = [
     // "eowymDzjzrDw-YfpzdJgJs:APA91bFpIgAosp6Z3SgHZr59sr2Juo9uJanEE7WtSw7Chp-bgNFcGyefVETfYJ1fuKhTDcmDmdtYd8TuyeALS_MkfIIyC-FU4q5gRfZCF6YHWkdfASqiE1Oul1NsS5GSFh8-hu0n545G",//huu
-    "cg9vLKO8RFaMBsW9LxELkB:APA91bFF0PM8MZOFQ_MQ4tcrEC7c6pNIRQ9s4JM7O388T75wfa5gEHDX8Il9MV7Bw79qh_MrcIJiWZ_grOcXwZ2O14SfRQMyCPDyNllJ_bNOevcig1hdh4ZvgcTBWEIIiaMNX0CQmLV0",//bao
-    "cS8_GueEQwm9boMEImnR3e:APA91bHwBuufX38B9s-rOxoX0BySzy5Ug2M_4QkGwp7LcMLhWPdwB4raS7gQHJC4MWYTdA1K8ay5f-9ck_WAqzrRCXBV9sl6C4ksTKi0x3BkCg4kyQz19z3gjhRaQgwprcdBxrLBFsML",//dth cua huu
+    "cg9vLKO8RFaMBsW9LxELkB:APA91bFF0PM8MZOFQ_MQ4tcrEC7c6pNIRQ9s4JM7O388T75wfa5gEHDX8Il9MV7Bw79qh_MrcIJiWZ_grOcXwZ2O14SfRQMyCPDyNllJ_bNOevcig1hdh4ZvgcTBWEIIiaMNX0CQmLV0", //bao
+    "cS8_GueEQwm9boMEImnR3e:APA91bHwBuufX38B9s-rOxoX0BySzy5Ug2M_4QkGwp7LcMLhWPdwB4raS7gQHJC4MWYTdA1K8ay5f-9ck_WAqzrRCXBV9sl6C4ksTKi0x3BkCg4kyQz19z3gjhRaQgwprcdBxrLBFsML", //dth cua huu
+    "emjevywaQ2W8L5wOMVL9bO:APA91bFgJqBvd4QDCF_Ng1h8uw5zhUfFLU3ICA-rKfLiEK9gA2hn3oHjrc4hl58Ttu1C5ChmYC-q2m3amNjAmFoqlhR4d9guCfsncxjRuEw0T3-eeEl44lyuOn0rMn6mWoNbfe8p-05N", //
   ];
   admin
     .messaging()
@@ -144,7 +145,10 @@ const fcm_customer_Topic = (contentBody) => {
     .messaging()
     .send(message)
     .then((response) => {
-      console.log("________Push notification đã được gửi thành công:", response);
+      console.log(
+        "________Push notification đã được gửi thành công:",
+        response
+      );
     })
     .catch((error) => {
       console.log("________Gửi push notification thất bại:", error);
@@ -163,7 +167,10 @@ const fcm_creator_Topic = (contentBody) => {
     .messaging()
     .send(message)
     .then((response) => {
-      console.log("________Push notification đã được gửi thành công:", response);
+      console.log(
+        "________Push notification đã được gửi thành công:",
+        response
+      );
     })
     .catch((error) => {
       console.log("________Gửi push notification thất bại:", error);
@@ -182,7 +189,10 @@ const fcm_uicha_Topic = (contentBody) => {
     .messaging()
     .send(message)
     .then((response) => {
-      console.log("________Push notification đã được gửi thành công:", response);
+      console.log(
+        "________Push notification đã được gửi thành công:",
+        response
+      );
     })
     .catch((error) => {
       console.log("________Gửi push notification thất bại:", error);
